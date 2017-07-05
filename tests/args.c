5a6fb732cc6da93791b503be4db63009a7779646
Sean MacLennan <seanm@seanm.ca>
2017-07-05 18:35:48 -0400

    Beef up the args tests.

--- tests/args.c
+++ tests/args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "../samlib.h"
 
 #ifdef TESTALL
@@ -9,13 +10,36 @@ int main(int argc, char *argv[])
 {
 	uint64_t size;
 	unsigned result;
+	unsigned long seconds;
 	char ch;
 	int rc = 0;
+	char str[24];
 
-	size = 2ULL << 40;
+	size = get_mem_len("2t");
 	result = nice_mem_len(size, &ch);
-	if (result != 2 || ch != 'T') {
+	if (size != (2ULL << 40) || result != 2 || ch != 'T') {
 		printf("Problems with nice_mem_len()\n");
+		rc = 1;
+	}
+
+	seconds = get_duration("30d");
+	nice_duration(seconds, str, sizeof(str));
+	if (seconds != 2592000UL || strcmp(str, "30d")) {
+		printf("Problems with 30d: %lu !=  2592000, %s != 30d\n",
+			   seconds, str);
+		rc = 1;
+	}
+
+	seconds = get_duration("1.2h");
+	nice_duration(seconds, str, sizeof(str));
+	if (seconds != 4320 || strcmp(str, "1h12m")) {
+		printf("Problems with 1.2h: %lu != 4320, %s != 1h12m\n",
+			   seconds, str);
+		rc = 1;
+	}
+
+	if (strcmp(nice_number(100000000UL), "100,000,000")) {
+		puts("Problems with nice_number");
 		rc = 1;
 	}
 
