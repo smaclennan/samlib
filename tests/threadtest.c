dae808c3466bd039276a693422e2fcaf095bd513
Sean MacLennan <seanm@seanm.ca>
2017-07-14 18:55:10 -0400

    Add a priority test.

--- tests/threadtest.c
+++ tests/threadtest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "../samthread.h"
+#include <sys/resource.h>
 
 #define CHILDREN  10
 
@@ -8,6 +9,66 @@ static mutex_t biglock;
 #else
 static DEFINE_MUTEX(biglock);
 #endif
+
+/* Shared with parent */
+static int child_was, child_is;
+static int done;
+/* */
+
+static int lower(void *arg)
+{
+	child_was = getpriority(PRIO_PROCESS, 0);
+
+	if (setpriority(PRIO_PROCESS, 0, child_was + 10)) {
+		printf("Unable to set priority\n");
+		done = 1;
+		return 1;
+	}
+
+	child_is = getpriority(PRIO_PROCESS, 0);
+
+	done = 1;
+
+	while (done == 1)
+		usleep(1000);
+
+	return 0;
+}
+
+/* This may be Linux specific */
+static int test_priority(void)
+{
+	samthread_t tid;
+	int parent_was, parent_is, parent_now;
+
+	parent_was = getpriority(PRIO_PROCESS, 0);
+
+	tid = samthread_create(lower, NULL);
+	if (tid == (samthread_t)-1) {
+		printf("samthread_create failed\n");
+		return 1;
+	}
+
+	while (done == 0)
+		usleep(1000);
+
+	parent_is = getpriority(PRIO_PROCESS, 0);
+
+	done = 2;
+
+	samthread_join(tid);
+
+	parent_now = getpriority(PRIO_PROCESS, 0);
+
+	/* We expect 0 for everybody except child_is */
+	if (parent_was || parent_is || parent_now || child_was || child_is != 10) {
+		printf("Parent was %d is %d now %d Child was %d is %d\n",
+			   parent_was, parent_is, parent_now, child_was, child_is);
+		return 1; /* test failed */
+	}
+
+	return 0;
+}
 
 static int fn(void *arg)
 {
@@ -56,5 +117,5 @@ static int thread_main(void)
 		printf("joined %ld rc %d\n", i, rc);
 	}
 
-	return 0;
+	return test_priority();
 }
