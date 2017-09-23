44a0a3de3b2dfc0b64e2411bd9da260e6a159486
Sean MacLennan <seanm@seanm.ca>
2017-09-22 23:27:32 -0400

    Add a test for timing mutex calls. Really only useful for a) Linux slim mutex or b) comparing OS mutex calls. Although Linux slim mutex has a huge advantage since we can gut the unlock.

---
+++ tests/mutex-timing.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "../samthread.h"
+#include "../samlib.h"
+
+/* Not part of testall. This is meant for checking optimizations, not for general runs.
+ * This only tests the non-contested case.
+ */
+
+#define LOOPS 10000000
+
+int main(int argc, char *argv[])
+{
+	long i;
+	struct timeval start, end;
+
+	mutex_t *mutex = mutex_create();
+	if (!mutex) {
+		printf("Unable to create mutex\n");
+		return 1;
+	}
+
+	gettimeofday(&start, NULL);
+	for (i = 0; i < LOOPS; ++i) {
+		mutex_lock(mutex);
+#if defined(__linux__) && !defined(WANT_PTHREADS)
+		mutex->state = 0;
+#else
+		mutex_unlock(mutex);
+#endif
+	}
+	gettimeofday(&end, NULL);
+
+	printf("mutex %ld\n", delta_timeval(&start, &end));
+
+	return 0;
+}
