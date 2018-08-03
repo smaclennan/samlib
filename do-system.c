a3dd40bf2a10f72448ed1d3459e05f8a0c66d469
Sean MacLennan <seanm@seanm.ca>
2018-08-03 18:44:05 -0400

    Report an error on buffer overflow

--- do-system.c
+++ do-system.c
@@ -1,18 +1,23 @@
+#include <stdarg.h>
 #ifndef WIN32
 #include <sys/wait.h>
 #endif
 #include "samlib.h"
 
 /* Do a system() call with var args. The cmd is limited to 1k. Returns
- * the system() status.
+ * either < 0 on error or the system status.
  */
 int do_system(const char *fmt, ...)
 {
 	char cmd[1024];
+	int n;
 	va_list ap;
 	va_start(ap, fmt);
-	vsnprintf(cmd, sizeof(cmd), fmt, ap);
+	n = vsnprintf(cmd, sizeof(cmd), fmt, ap);
 	va_end(ap);
+	
+	if (n >= sizeof(cmd))
+		return -EINVAL;
 
 	int rc = system(cmd);
 #ifndef WIN32
