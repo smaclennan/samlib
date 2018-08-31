63671f44e5527eafd01003bcc3646140359e59d2
Sean MacLennan <seanm@seanm.ca>
2018-08-31 18:28:21 -0400

    strlcat

--- safecpy.c
+++ safecpy.c
@@ -65,4 +65,30 @@ size_t strlcpy(char *dst, const char *src, size_t dstsize)
 
 	return srcsize;
 }
+
+/* A simple strlcat implementation for Linux */
+size_t strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i = 0;
+
+	while (dstsize > 0 && *dst) {
+		--dstsize;
+		++dst;
+		++i;
+	}
+
+	if (dstsize > 0) {
+		--dstsize;
+		while (*src && i < dstsize) {
+			*dst++ = *src++;
+			++i;
+		}
+		*dst = 0;
+	}
+
+	/* strlcat returns the size of the src + initial dst */
+	while (*src++) ++i;
+
+	return i;
+}
 #endif
