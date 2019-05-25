ec823b8bc066a57aefddcd876f860db179098d5f
Sean MacLennan <seanm@seanm.ca>
2019-05-25 11:23:54 -0400

    Simplify

--- tea.c
+++ tea.c
@@ -47,25 +47,24 @@ int tea_bag_size(int len)
 	return len;
 }
 
-static void decrypt_block(const uint32_t *k, uint32_t *v)
-{
-	uint32_t sum = 0xC6EF3720;
-	uint32_t v0 = v[0], v1 = v[1];
-
-	for (int i = 0; i < 32; i++) {
-		v1 -= ((v0 << 4) + k[2]) ^ (v0 + sum) ^ ((v0 >> 5) + k[3]);
-		v0 -= ((v1 << 4) + k[0]) ^ (v1 + sum) ^ ((v1 >> 5) + k[1]);
-		sum -= TEA_CONSTANT;
-	}
-
-	v[0] = v0; v[1] = v1;
-}
-
 void tea_decrypt(const void *key, void *data, int len)
 {
+	const uint32_t *k = key;
+	uint32_t *v = data;
+
 	while (len >= TEA_BAG_SIZE) {
-		decrypt_block(key, data);
-		data += TEA_BAG_SIZE;
+		uint32_t sum = 0xC6EF3720;
+		uint32_t v0 = v[0], v1 = v[1];
+
+		for (int i = 0; i < 32; i++) {
+			v1 -= ((v0 << 4) + k[2]) ^ (v0 + sum) ^ ((v0 >> 5) + k[3]);
+			v0 -= ((v1 << 4) + k[0]) ^ (v1 + sum) ^ ((v1 >> 5) + k[1]);
+			sum -= TEA_CONSTANT;
+		}
+
+		v[0] = v0; v[1] = v1;
+
+		v += 2;
 		len -= TEA_BAG_SIZE;
 	}
 }
